bf12b48d367bf6ab5bbc8dc2600bfffa37c2fdcf
Trideep555 <trideep555@gmail.com>
2023-12-28 21:08:30 +0530

    Login Logout

--- Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -29,7 +29,7 @@ namespace CodeWithMe.Areas.Identity.Pages.Account
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
-                return LocalRedirect(returnUrl);
+                return LocalRedirect("/UI");
             }
             else
             {
